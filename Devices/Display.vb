e79a9b951eb23d582df8fe1ef00c8c62895f97a2
Barret <format.e.drive@gmail.com>
2017-08-02 23:32:19 +0200

    Reduced flickering

--- Devices/Display.vb
+++ Devices/Display.vb
@@ -1,48 +1,50 @@
-﻿Imports System.Drawing
-Imports System.Drawing.Imaging
+﻿Imports System.Drawing.Imaging
+Imports System.Drawing.Drawing2D
 
 Public Class Display
     Inherits Device
     Public Const Width As Integer = 64
     Public Const Height As Integer = 32
     Public Const Resolution As Integer = &H6
-    Public Property Invert As Boolean
     Public Property Redraw As Boolean
-    Public Property buffer As Integer(,)
+    Public Property buffer As Int16(,)
     Public Property Background As Brush
     Public Property Foreground As Brush
     Sub New(Machine As Machine)
         MyBase.New(Machine, "Display")
-        Me.Invert = False
         Me.Redraw = False
-        Me.Background = New SolidBrush(Color.FromArgb(139, 255, 0))
+        Me.Background = New SolidBrush(Color.FromArgb(140, 255, 0))
         Me.Foreground = New SolidBrush(Color.FromArgb(0, 0, 0))
-        Me.buffer = New Integer(Display.Width - 1, Display.Height - 1) {}
+        Me.buffer = New Int16(Display.Width - 1, Display.Height - 1) {}
         Me.Clear()
     End Sub
     Public Function Draw(x As Integer, y As Integer, buffer As Integer()) As Byte
-        Dim px As Integer = x, py As Integer = y, value As Byte = &H0
+        Dim px As Integer = x, py As Integer = y, value As Byte = &H0, changed As Boolean = False
         For i As Integer = 0 To buffer.Length - 1
-            Dim line As Char() = Me.BitToString(buffer(i))
             For pixel As Integer = 0 To 7
-                If line(pixel) = Char.Parse("1") Then
+                If Me.BitToString(buffer(i))(pixel) = Char.Parse("1") Then
                     If px + pixel < Width AndAlso py + i < Height Then
                         If Me.buffer(px + pixel, py + i) = &H1 Then
                             value = &H1
+                            changed = True
                         End If
-                        Me.buffer(px + pixel, py + i) = Me.buffer(px + pixel, py + i) Xor &H1
+                        Me.buffer(px + pixel, py + i) = CShort(Me.buffer(px + pixel, py + i) Xor &H1)
                     End If
                 End If
             Next
         Next
-        Me.Redraw = True
+        If (Not changed) Then Me.Redraw = True
         Return value
     End Function
     Public Sub DrawFrame()
-        SyncLock Me.Machine.Viewport
-            Using bm As New Bitmap(Me.Machine.Viewport.Size.Width, Me.Machine.Viewport.Size.Height)
+        Using bm As New Bitmap(Me.Machine.Viewport.Size.Width, Me.Machine.Viewport.Size.Height)
+            SyncLock bm
                 Using g As Graphics = Graphics.FromImage(bm)
                     g.Clear(Color.FromArgb(139, 255, 0))
+                    g.SmoothingMode = SmoothingMode.None
+                    g.CompositingMode = CompositingMode.SourceCopy
+                    g.PixelOffsetMode = PixelOffsetMode.Half
+                    g.InterpolationMode = InterpolationMode.NearestNeighbor
                     Dim pixel As Integer = 0
                     For y As Integer = 0 To Display.Height - 1
                         For x As Integer = 0 To Display.Width - 1
@@ -52,8 +54,8 @@ Public Class Display
                     Next
                 End Using
                 Me.Machine.Viewport.Image = CType(bm.Clone, Image)
-            End Using
-        End SyncLock
+            End SyncLock
+        End Using
     End Sub
     Public Sub Clear()
         For y As Integer = 0 To Height - 1
